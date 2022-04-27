4ebdd4d3720e78fe7b5ea4f05be50ec6f99c0258
pepo <pepo.ospina@gmail.com>
2022-04-27 05:25:10 -0400

    added deploy script

--- packages/contracts/deploy/00_deploy_factory.ts
+++ packages/contracts/deploy/00_deploy_factory.ts
@@ -2,9 +2,23 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  // const { getNamedAccounts, deployments } = hre;
-  // const { deploy } = deployments;
-  // const { deployer } = await getNamedAccounts();
+  const { getNamedAccounts, deployments } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const campaign = await deploy('Campaign', {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [],
+    log: true,
+  });
+
+  await deploy('CampaignFactory', {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [campaign.address],
+    log: true,
+  });
 };
 
 export default func;
