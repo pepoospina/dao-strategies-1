f9820de6fc05b2614856553608bae2f8dbc59071
pepo <pepo.ospina@gmail.com>
2022-05-12 15:38:51 +0200

    copy paste strategy file

--- packages/core/src/strategies/github/prs-reactions-weighed/index.ts
+++ packages/core/src/strategies/github/prs-reactions-weighed/index.ts
@@ -12,25 +12,13 @@ interface Params {
   timeRange: { start: number; end: number };
 }
 
-const strategy: Strategy = async (
-    world: World,
-    params: Params
-) => {
-    if (params.timeRange.start >= params.timeRange.end) {
-        throw new Error("time params incorrect: start must be smaller than end");
-    }
+const strategy: Strategy = async (world: World, params: Params) => {
+  if (params.timeRange.start >= params.timeRange.end) {
+    throw new Error('time params incorrect: start must be smaller than end');
+  }
 
-    let reactionsPerContributor = new Map<string, Array<number>>();
-    let contributors = new Array<string | undefined>() // convert to set
-
-    // get all contributors in the repositories
-    for (const repo of params.repositories) {
-        let repoContributors = await getRepoContributors(world, repo);
-        for (const contributor of repoContributors) {
-            contributors.push(contributor);
-        }
-    }
-    contributors = [...new Set(contributors)]; // remove duplicates
+  const reactionsPerContributor = new Map<string, Array<number>>();
+  let contributors = new Array<string | undefined>();
 
   // get all contributors in the repositories
   for (const repo of params.repositories) {
@@ -39,39 +27,37 @@ const strategy: Strategy = async (
   }
   contributors = [...new Set(contributors)]; // remove duplicates
 
-    for (const repo of params.repositories) {
-        // get all pulls that were created and merged at the specified time range 
-        const allPulls = await getPrsInRepo(world, repo);
-        const pullsFiltered = allPulls.filter(function (pull) {
-            if (pull.merged_at == null) {
-                return false;
-            }
-            return (
-                toTimeStamp(pull.merged_at) >= params.timeRange.start &&
-                toTimeStamp(pull.merged_at) <= params.timeRange.end);
-        });
+  for (const repo of params.repositories) {
+    // get all pulls that were created and merged at the specified time range
+    const allPulls = await getPrsInRepo(world, repo);
+    const pullsFiltered = allPulls.filter(function (pull) {
+      if (pull.merged_at == null) {
+        return false;
+      }
+      return (
+        toTimeStamp(pull.created_at) >= params.timeRange.start &&
+        toTimeStamp(pull.created_at) <= params.timeRange.end &&
+        toTimeStamp(pull.merged_at) >= params.timeRange.start &&
+        toTimeStamp(pull.merged_at) <= params.timeRange.end
+      );
+    });
 
-        // get the amount of reactions on every pull request that was made by a contributor
-        for (const pull of pullsFiltered) {
-            if (pull.user == null) {
-                continue;
-            }
-            const pullCreator = pull.user.login;
-            let reactionsNum = 0;
-            const reactions = await getPullReactions(world, repo, pull.number);
-            for (const reaction of reactions) {
-                if (contributors.includes(reaction.user?.login) && // only reactions by contributors
-                    reaction.content == '+1') { // only "thumbs up" or "rocket" or "heart" or "celebration" reactions count
-                    reactionsNum += 1;
-                }
-            }
-
-            if (reactionsPerContributor.has(pullCreator)) {
-                reactionsPerContributor.get(pullCreator)?.push(reactionsNum);
-            }
-            else {
-                reactionsPerContributor.set(pullCreator, [reactionsNum])
-            }
+    // get the amount of reactions on every pull request that was made by a contributor
+    for (const pull of pullsFiltered) {
+      if (pull.user == null) {
+        continue;
+      }
+      const pullCreator: string = pull.user.login;
+      let reactionsNum = 1; // every pull has one default reaction
+      const reactions = await getPullReactions(world, repo, pull.number);
+      for (const reaction of reactions) {
+        if (
+          reaction.user?.login !== pullCreator && // reaction wasn't made by the creator of the pull
+          contributors.includes(reaction.user?.login) && // only reactions by contributors
+          reaction.content === '+1'
+        ) {
+          // only "thumbs up" reactions count
+          reactionsNum += 1;
         }
       }
 
@@ -94,7 +80,7 @@ const strategy: Strategy = async (
     );
   }
 
-    return scores; // need to normalize
+  return scores;
 };
 
 export type { Params };
