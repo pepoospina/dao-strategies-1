2e72db6100593d4f5f871fadeab0bb7e83ec8b92
Ido <gershido@gmail.com>
2022-05-12 09:38:48 +0300

    support multiple repos

--- packages/core/src/strategies/github/prs-reactions-weighed/index.ts
+++ packages/core/src/strategies/github/prs-reactions-weighed/index.ts
@@ -15,40 +15,50 @@ const strategy: Strategy = async (
         throw new Error("time params incorrect: start must be smaller than end");
     }
 
-    // get all pulls that were created and merged at the specified time range
-    const allPulls = await getPrsInRepo(world, params.repositories[0]);
-    const pullsFiltered = allPulls.filter(function (pull) {
-        if (pull.merged_at == null) {
-            return false;
-        }
-        return (toTimeStamp(pull.created_at) >= params.timeRange.start &&
-            toTimeStamp(pull.created_at) <= params.timeRange.end &&
-            toTimeStamp(pull.merged_at) >= params.timeRange.start &&
-            toTimeStamp(pull.merged_at) <= params.timeRange.end);
-    });
 
-    // get all contributors for the repo
-    const contributors = await getRepoContributors(world, params.repositories[0]);
-
-    // get the amount of reactions on every pull request that was made by a contributor
     let reactionsPerContributor = new Map<string, Array<number>>();
-    for (const pull of pullsFiltered) {
-        const pullCreator: string = pull.user.login;
-        let reactionsNum = 1; // every pull has one default reaction 
-        const reactions = await getPullReactions(world, params.repositories[0], pull.number);
-        for (const reaction of reactions) {
-            if (reaction.user?.login != pullCreator && // reaction wasn't made by the creator of the pull
-                contributors.includes(reaction.user?.login) && // only reactions by contributors
-                reaction.content == '+1') { // only "thumbs up" reactions count
-                reactionsNum += 1;
-            }
-        }
+    let contributors = new Array<string | undefined>()
 
-        if (reactionsPerContributor.has(pullCreator)) {
-            reactionsPerContributor.get(pullCreator)?.push(reactionsNum);
-        }
-        else {
-            reactionsPerContributor.set(pullCreator, [reactionsNum])
+    // get all contributors in the repositories
+    for (const repo of params.repositories) {
+        let repoContributors = await getRepoContributors(world, repo);
+        contributors.concat(repoContributors);
+    }
+    contributors = [...new Set(contributors)]; // remove duplicates
+
+
+    for (const repo of params.repositories) {
+        // get all pulls that were created and merged at the specified time range 
+        const allPulls = await getPrsInRepo(world, repo);
+        const pullsFiltered = allPulls.filter(function (pull) {
+            if (pull.merged_at == null) {
+                return false;
+            }
+            return (toTimeStamp(pull.created_at) >= params.timeRange.start &&
+                toTimeStamp(pull.created_at) <= params.timeRange.end &&
+                toTimeStamp(pull.merged_at) >= params.timeRange.start &&
+                toTimeStamp(pull.merged_at) <= params.timeRange.end);
+        });
+
+        // get the amount of reactions on every pull request that was made by a contributor
+        for (const pull of pullsFiltered) {
+            const pullCreator: string = pull.user.login;
+            let reactionsNum = 1; // every pull has one default reaction 
+            const reactions = await getPullReactions(world, repo, pull.number);
+            for (const reaction of reactions) {
+                if (reaction.user?.login != pullCreator && // reaction wasn't made by the creator of the pull
+                    contributors.includes(reaction.user?.login) && // only reactions by contributors
+                    reaction.content == '+1') { // only "thumbs up" reactions count
+                    reactionsNum += 1;
+                }
+            }
+
+            if (reactionsPerContributor.has(pullCreator)) {
+                reactionsPerContributor.get(pullCreator)?.push(reactionsNum);
+            }
+            else {
+                reactionsPerContributor.set(pullCreator, [reactionsNum])
+            }
         }
     }
 
